1f5cf0a133e7366daa5d088685ce0a3967845714
Stevemuigai2024 <Stevemuigai2024@gmail.com>
2025-01-10 01:34:06 +0300

    Update telegram_paypal_bot.py

--- telegram_paypal_bot.py
+++ telegram_paypal_bot.py
@@ -1,4 +1,4 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Bot  # Added Bot import
 from telegram.ext import (
     Application, CommandHandler, CallbackQueryHandler, ContextTypes
 )
