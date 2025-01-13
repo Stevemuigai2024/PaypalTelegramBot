0d0838b4d42755f5f0bea142138f4e7601190246
Stevemuigai2024 <Stevemuigai2024@gmail.com>
2025-01-14 01:05:45 +0300

    Update telegram_paypal_bot.py

--- telegram_paypal_bot.py
+++ telegram_paypal_bot.py
@@ -9,7 +9,7 @@ from telegram.request import HTTPXRequest
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(name)s - %(levelname=s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
